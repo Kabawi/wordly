e4b262b1d58727c7dde9e64444f2c7d33df62193
Callum Worrall <callum.worrall71@gmail.com>
2020-09-21 14:52:52 +1000

    Clean up and beginning to add dropdown selection for categories.

--- modules/webNavigator/bubble.js
+++ modules/webNavigator/bubble.js
@@ -1,8 +1,13 @@
-
-
 //Bubble Specs
 let size = {width: 170, height: 75}
 let backgroundColour = "white";
+
+let firstRadiusCount = 6;
+let firstRadiusDegrees = 360 / firstRadiusCount;
+
+let secondRadiusCount = 8;
+let secondRadiusDegrees = 360 / secondRadiusCount;
+
 
 export function createBubble(draw, value, pos, focusing) {
     //Draw Bubble
@@ -32,25 +37,71 @@ function drawLabel(draw, value, ellipse) {
     return label;
 }
 
-export function findPositionAroundFocus(radius, focusBubble, degrees, index, offset) {
+export function createAssociateBubbles(draw, focusBubble, associations) {
+    let associateBubbles = []
+
+    if(associations.length > 0) {
+        let radiusDegrees = firstRadiusDegrees
+        
+        if(associations.length < firstRadiusCount) {
+            radiusDegrees = 360 / associations.length;
+        } else if (associations.length < firstRadiusCount + secondRadiusCount) {
+            radiusDegrees = 360 / (associations.length - firstRadiusCount);
+        }
+
+        associations.forEach((association, i) => {
+            let position = {};
+
+            //First Radius
+            if(i < firstRadiusCount) {
+                if(associations.length < firstRadiusCount){
+                    position = findPositionAroundFocus(150, focusBubble, radiusDegrees, i, 1.5, 0, 90);
+                } else {
+                    position = findPositionAroundFocus(150, focusBubble, firstRadiusDegrees, i, 1.5, 0, 90);
+                }
+                associateBubbles.push(createBubble(draw, association, position, false));
+            }
+
+            // Second Radius
+            else if (i < (firstRadiusCount + secondRadiusCount)) {
+                if (associations.length < firstRadiusCount + secondRadiusCount) {
+                    position = findPositionAroundFocus(280, focusBubble, radiusDegrees, i, 1.4, 0, 90);
+                } else {
+                    position = findPositionAroundFocus(280, focusBubble, secondRadiusDegrees, i, 0, 0, 90);
+                }
+                associateBubbles.push(createBubble(draw, association, position, false));
+
+            } else {
+                return associateBubbles;
+            }
+        })
+
+        return associateBubbles;
+    }
+}
+
+export function findPositionAroundFocus(radius, focusBubble, degrees, index, xOffset = 0, yOffset = 0, angleOffset = 0) {
     let x = 0;
     let y = 0;
     let i = index + 1;
 
-    let horizontalExtension = 1.5;
+    let xRadius = radius;
+    let yRadius = radius
 
-    if(offset == null) {
-        x = (radius * horizontalExtension) *  Math.cos(getRadians(degrees * i)) + (focusBubble.ellipse.x() + focusBubble.ellipse.width() / 2);
-        y = radius *  Math.sin(getRadians(degrees * i)) + (focusBubble.ellipse.y() + focusBubble.ellipse.height() / 2);
+    if(xOffset != 0) {
+        xRadius *= xOffset;
     }
-    else {
-        x = (radius * horizontalExtension) *  Math.cos(getRadians(offset + (degrees * i))) + (focusBubble.ellipse.x() + focusBubble.ellipse.width() / 2);
-        y = radius *  Math.sin(getRadians(offset + (degrees * i))) + (focusBubble.ellipse.y() + focusBubble.ellipse.height() / 2);
-    
+
+    if(yOffset != 0) {
+        yRadius *= yOffset;
     }
+
+    x = xRadius * Math.cos(getRadians(angleOffset + degrees * i)) + (focusBubble.ellipse.x() + focusBubble.ellipse.width() / 2);
+    y = yRadius * Math.sin(getRadians(angleOffset + degrees * i)) + (focusBubble.ellipse.y() + focusBubble.ellipse.height() / 2);
 
     return {x: x, y: y};
 }
+
 
 function getRadians(degrees) {
     let radians = degrees * Math.PI;
--- modules/webNavigator/webNavigator.js
+++ modules/webNavigator/webNavigator.js
@@ -1,21 +1,17 @@
 //Navigator Specs
-import { createBubble, findPositionAroundFocus } from './bubble.js';
+import { createBubble, createAssociateBubbles } from './bubble.js';
 
 let navigatorSize = {width: 1200, height: 800};
 let draw = SVG('navigator').size(navigatorSize.width, navigatorSize.height);
 let backgroundColour = "white"
-
-let firstRadiusCount = 6;
-let firstRadiusDegrees = 360 / firstRadiusCount;
-
-let secondRadiusCount = 8;
-let secondRadiusDegrees = 360 / secondRadiusCount;
+let border;
 
 let focusBubble = {};
 let focusPosition = {x: navigatorSize.width / 2, y: navigatorSize.height / 2};
 
 let associationCategory = '';
 let associations = []
+let associateBubbles = []
 
 //Temp Variables
 let tempFocusWord = {
@@ -43,84 +39,34 @@ let tempFocusWord = {
 }
 
 export function drawNavigator() {
-    let border = draw.polyline('0,0 0,800, 1200,800, 1200,0 0,0').fill(backgroundColour).stroke({width: 4, color: "black"});
-
-    focusBubble = createBubble(draw, tempFocusWord, focusPosition, true);
-
-    associationCategory = "synonyms";
-    associations = getAssociations(tempFocusWord, associationCategory);
-
-    if(associations != null) {
-        createRelations(focusBubble, associations)
-    }
+    associationCategory = 'synonyms';
+    setFocus(tempFocusWord);
 }
 
-//////      Category Relations         //////
-
 export function setFocus(newWord) {
-    // console.log('Changed focus word to ' + newWord.word)
-
-    // console.log(newWord)
-    
     draw.clear();
+    border = draw.polyline('0,0 0,800, 1200,800, 1200,0 0,0').fill(backgroundColour).stroke({width: 4, color: "black"});
     focusBubble = createBubble(draw, newWord, focusPosition, true);
-    createRelations(focusBubble, getAssociations(newWord, associationCategory));
+
+    associations = getAssociations(newWord, associationCategory);
+    if(associations != null) {
+        associateBubbles = createAssociateBubbles(draw, focusBubble, associations);
+    }    
 }
 
 function getAssociations(word, category) {
-    let associations = []
-
+    let newAssociations = []
     if(word.results.length > 0) {
         word.results.forEach((result) => {
             if(result[category] != null && result[category].length > 0) {
                 result[category].forEach((element) => {
-                    associations.push(element);
+                    newAssociations.push(element);
                 })
             }
         })
 
-        associations = [...new Set(associations)]
-        console.log(associations);
-        return associations;
-    }
-}
-
-function createRelations(focusBubble, associations) {
-    if(associations.length > 0) {
-        let radiusDegrees = firstRadiusDegrees
-        
-        if(associations.length < firstRadiusCount) {
-            radiusDegrees = 360 / associations.length;
-        } else if (associations.length < firstRadiusCount + secondRadiusCount) {
-            radiusDegrees = 360 / (associations.length - firstRadiusCount);
-        }
-
-        associations.forEach((association, i) => {
-            let position = {};
-
-            //First Radius
-            if(i < firstRadiusCount) {
-                if(associations.length < firstRadiusCount){
-                    position = findPositionAroundFocus(150, focusBubble, radiusDegrees, i, 90);
-                } else {
-                    position = findPositionAroundFocus(150, focusBubble, firstRadiusDegrees, i, 90);
-                }
-                createBubble(draw, association, position, false);
-            }
-
-            // Second Radius
-            else if (i < (firstRadiusCount + secondRadiusCount)) {
-                if (associations.length < firstRadiusCount + secondRadiusCount) {
-                    console.log("hit")
-                    position = findPositionAroundFocus(300, focusBubble, radiusDegrees, i, 90);
-                } else {
-                    position = findPositionAroundFocus(300, focusBubble, secondRadiusDegrees, i, 90);
-                }
-                createBubble(draw, association, position, false);
-
-            } else {
-                return;
-            }
-        })
+        newAssociations = [...new Set(newAssociations)]
+        console.log(newAssociations);
+        return newAssociations;
     }
 }
