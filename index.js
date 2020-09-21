effab875acf34d3401108a6b47e0f4809aa2571a
Luke-kb <keananbrown@gmail.com>
2020-09-21 11:12:31 +1000

    added dataRetrieval.js, imported into index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-
 import { blue, red, navigator } from './modules/webNavigator.js';
+import { getWordData } from './modules/webNavigator.js';
 
+getWordData('manifest');
 navigator();
 
 console.log(`${blue} ${red}`)
---
+++ modules/dataRetrieval.js
@@ -0,0 +1,51 @@
+import { API_KEY } from './modules/keys.js';
+
+function getWordData(word) {
+
+    const results = 
+        url: `https://wordsapiv1.p.rapidapi.com/words/${word}`,
+        method: 'GET',
+        headers: {
+        'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+        'x-rapidapi-key': API_KEY
+    }
+
+    axios(results) 
+    .then(response => {
+        const data = response.data
+        console.log(data)
+        console.log(data.word)
+       
+        //identify title of word
+        let wordTitle = document.getElementById("wordTitle")
+        //display title on page
+        wordTitle.textContent = data.word
+        
+        //identify wordDef element
+        let wordDef = document.getElementById("wordDef")
+        //display data if available
+        if (data.results) {
+            console.log(data.results[0].definition)
+            wordDef.textContent = `Definition: ${data.results[0].definition}`
+        } else {
+            wordDef.textContent = ''
+        }
+        //identify wordSyllable element
+        let wordSyllables = document.getElementById("wordSyllables")
+        //display data if available
+        if (data.syllables) {
+            console.log(data.syllables.count)
+            wordSyllables.textContent = `number of syllables: ${data.syllables.count}`
+            } else {
+            wordSyllables.textContent = ''
+            }
+        //display frequency element
+        let wordFreq = document.getElementById('wordFreq')
+        //display data if available
+        if (data.frequency) {
+            wordFreq = textContent = `Frequency: ${data.frequency}`
+        } else {
+            wordFreq = ''
+        }
+    })  
+}
