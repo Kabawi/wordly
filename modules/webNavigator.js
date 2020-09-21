fb37cacf90c56545323e006a4d7d127a8307943c
Callum Worrall <callum.worrall71@gmail.com>
2020-09-21 10:47:04 +1000

    Drawing word label and create a 'bubbleObject'

--- modules/webNavigator.js
+++ modules/webNavigator.js
@@ -9,26 +9,37 @@
 //Navigator Specs
 let navigatorSize = {width: 1200, height: 800};
 let draw = SVG('navigator').size(navigatorSize.width, navigatorSize.height);
+let backgroundColour = "white"
 
 
 //Bubble Specs
 let bubbleSize = {width: 170, height: 75}
-let bubbleColor = "grey";
+let bubbleColour = "white";
 
 export function drawNavigator() {
-    let border = draw.polyline('0,0 0,800, 1200,800, 1200,0 0,0').fill("white").stroke({width: 4, color: "black"});
-    let focusBubble = createFocusBubble("word");
-}
+    let border = draw.polyline('0,0 0,800, 1200,800, 1200,0 0,0').fill(backgroundColour).stroke({width: 4, color: "black"});
 
-function createFocusBubble(focusWord) {
     let focusPos = {x: navigatorSize.width / 2, y: navigatorSize.height / 2};
-    let focusBubble = drawBubble(focusWord, focusPos);
+    let focusBubble = createBubble("tempWord", focusPos);
 }
 
-function drawBubble(word, pos) {
+function createBubble(word, pos) {
+    
+    //Draw Bubble
     let bubble = draw.ellipse(bubbleSize.width, bubbleSize.height)
     .move(pos.x - bubbleSize.width / 2, navigatorSize.height - pos.y - bubbleSize.height / 2)
-    .fill(bubbleColor)
+    .stroke({width: 2, color: "black"})
+    .fill(bubbleColour);
 
-    return bubble;
+    //Draw Label & Text
+    let label = drawLabel(word, bubble);
+
+    let bubbleObject = {bubble: bubble, label: label};
+    return bubbleObject;
+}
+
+function drawLabel(word, bubble) {
+    let label = draw.text(word)
+    .font({'size':'22px','anchor':'middle'})
+    .move(bubble.width() / 2 + bubble.x(), bubble.height() / 3 + bubble.y());
 }
