c112b3dd4f659fd26ba72c7a06732ba996603fd6
Callum Worrall <callum.worrall71@gmail.com>
2020-09-21 10:34:25 +1000

    Drawing Focus Bubble (SVG Ellipse).

--- modules/webNavigator.js
+++ modules/webNavigator.js
@@ -5,16 +5,30 @@
 // }
 // export { blue, red }
 
+
+//Navigator Specs
+let navigatorSize = {width: 1200, height: 800};
+let draw = SVG('navigator').size(navigatorSize.width, navigatorSize.height);
+
+
+//Bubble Specs
+let bubbleSize = {width: 170, height: 75}
+let bubbleColor = "grey";
+
 export function drawNavigator() {
-    let navigatorSize = {width: 1200, height: 800};
-    let draw = SVG('navigator').size(navigatorSize.width, navigatorSize.height);
-    // let corners = [];
     let border = draw.polyline('0,0 0,800, 1200,800, 1200,0 0,0').fill("white").stroke({width: 4, color: "black"});
-
+    let focusBubble = createFocusBubble("word");
 }
 
-function drawBubble() {
-    let bubbleColour = "grey"
-    // let focusBubble = draw.ellipse
+function createFocusBubble(focusWord) {
+    let focusPos = {x: navigatorSize.width / 2, y: navigatorSize.height / 2};
+    let focusBubble = drawBubble(focusWord, focusPos);
 }
 
+function drawBubble(word, pos) {
+    let bubble = draw.ellipse(bubbleSize.width, bubbleSize.height)
+    .move(pos.x - bubbleSize.width / 2, navigatorSize.height - pos.y - bubbleSize.height / 2)
+    .fill(bubbleColor)
+
+    return bubble;
+}
