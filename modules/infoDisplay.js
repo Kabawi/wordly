63eb96c4e9bd9adbda3b6bda13779ca9320f5e18
Luke-kb <keananbrown@gmail.com>
2020-09-23 11:19:06 +1000

    added wordType to each definition and pronunciation. small formatting changes

--- modules/infoDisplay.js
+++ modules/infoDisplay.js
@@ -1,20 +1,32 @@
 
 export function displayWordInfo(data) {
-    
-    const definitionsList = document.getElementById('definitionsList')
+    //define and clear definitions list for each new word
+    let definitionsList = document.getElementById('definitionsList')
     definitionsList.innerHTML = ""
-    document.getElementById("title").textContent = `${data.word}`
+    //set word title 
+    document.getElementById("title").textContent = data.word
+    document.getElementById("pronunce").textContent = data.pronunciation.all
     console.log(data)
 
     // display definition and partOfSpeech, adding each to an li element 
     data.results.forEach((result) => {
-        console.log(result.definition);
-        console.log(result.partOfSpeech);
-        let li = document.createElement("li")
+        // console.log(result.definition);
+        // console.log(result.partOfSpeech);
 
-        li.appendChild(document.createTextNode(`${result.partOfSpeech}: ${result.definition}`))
-        definitionsList.appendChild(li)
-        
+        //create individual li element
+        let listItem = document.createElement("li")
+        //create word type heading
+        let wordTypeDef = document.createElement("h6")
+        //create definition paragraph
+        let def = document.createElement("p")
+
+        // listItem.appendChild(document.createTextNode(`${result.partOfSpeech}: ${result.definition}`))
+        wordTypeDef.textContent = result.partOfSpeech
+        def.textContent = result.definition
+        //append each element to its parent
+        listItem.appendChild(wordTypeDef)
+        listItem.appendChild(def)
+        definitionsList.appendChild(listItem)
 
     })
     
