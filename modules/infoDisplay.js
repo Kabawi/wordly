70f6967ab5b2db1ff93235c669e08b64de151076
Luke-kb <keananbrown@gmail.com>
2020-09-23 10:44:18 +1000

    changed animation speed, changed added wordtype to infodisplay li

--- modules/infoDisplay.js
+++ modules/infoDisplay.js
@@ -4,14 +4,18 @@ export function displayWordInfo(data) {
     const definitionsList = document.getElementById('definitionsList')
     definitionsList.innerHTML = ""
     document.getElementById("title").textContent = `${data.word}`
-    // console.log(data)
+    console.log(data)
 
-    // display definition adding each to an li element 
+    // display definition and partOfSpeech, adding each to an li element 
     data.results.forEach((result) => {
-        // console.log(result.definition);
+        console.log(result.definition);
+        console.log(result.partOfSpeech);
         let li = document.createElement("li")
-        li.appendChild(document.createTextNode(result.definition))
+
+        li.appendChild(document.createTextNode(`${result.partOfSpeech}: ${result.definition}`))
         definitionsList.appendChild(li)
+        
+
     })
     
 }
