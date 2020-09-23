c60bb137129ec26519f4baa8d28c751cc57fbe4f
Callum Worrall <callum.worrall71@gmail.com>
2020-09-23 12:25:13 +1000

    Tweaked info display width.

--- modules/dataRetrieval.js
+++ modules/dataRetrieval.js
@@ -21,57 +21,6 @@ export function getWordData(word) {
         //Change Navigator Focus
         setWord(data);
         displayWordInfo(data);
-        // console.log(data)
-        // console.log(data.word)
-    
-        //identify title of word
-        // let wordTitle = document.getElementById("wordTitle")
-        //display title on page
-        // wordTitle.textContent = data.word
-        
-        //identify wordDef element
-        // let wordDef = document.getElementById("wordDef")
-        //display data if available
-        // if (data.results) {
-            // console.log(data.results[0].definition)
-            // wordDef.textContent = `Definition: ${data.results[0].definition}`
-        // } else {
-            // wordDef.textContent = ''
-        // }
-        //identify wordSyllable element
-        // let wordSyllables = document.getElementById("wordSyllables")
-        //display data if available
-        // //identify title of word
-        // let wordTitle = document.getElementById("wordTitle")
-        // //display title on page
-        // wordTitle.textContent = data.word
-        
-        // //identify wordDef element
-        // let wordDef = document.getElementById("wordDef")
-        // //display data if available
-        // if (data.results) {
-        //     console.log(data.results[0].definition)
-        //     wordDef.textContent = `Definition: ${data.results[0].definition}`
-        // } else {
-        //     wordDef.textContent = ''
-        // }
-        // //identify wordSyllable element
-        // let wordSyllables = document.getElementById("wordSyllables")
-        // //display data if available
-        // if (data.syllables) {
-        //     console.log(data.syllables.count)
-        //     wordSyllables.textContent = `number of syllables: ${data.syllables.count}`
-        //     } else {
-        //     wordSyllables.textContent = ''
-        //     }
-        //display frequency element
-        // let wordFreq = document.getElementById('wordFreq')
-        //display data if available
-        // if (data.frequency) {
-        //     wordFreq = textContent = `Frequency: ${data.frequency}`
-        // } else {
-        //     wordFreq = ''
-        // }
     })  
 
     .catch((error) => { 
